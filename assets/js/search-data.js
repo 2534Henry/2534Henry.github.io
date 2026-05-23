662c2caa55a64f83622db95576c6d89ef4458f9c
2534Henry <1022182060@qq.com>
2026-05-23 03:40:57 +0000

    Deploying to gh-pages from @ 2534Henry/2534Henry.github.io@fa3c2bcd35100f4b0bff8decebeef30743398c51 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -64,14 +64,14 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
+          window.open("/assets/pdf/CV_WangZihao_ZJU.pdf", "_blank");
         },
       },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75%72.%65%6D%61%69%6C@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%7A%69%68%61%6F.%77%61%6E%67@%7A%6A%75.%65%64%75.%63%6E", "_blank");
         },
       },{
         id: 'social-rss',
@@ -79,6 +79,13 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("/feed.xml", "_blank");
+        },
+      },{
+        id: 'social-github',
+        title: 'GitHub',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://github.com/Zwang414", "_blank");
         },
       },{
       id: 'light-theme',
